5bf1934a76b21a064b94febe485e55feafd9d75d
Christopher Bartling <chris.bartling@gmail.com>
2020-10-14 00:38:57 -0500

    Building out the sequential task set

--- task_sets/gummi_bears_task_set.py
+++ task_sets/gummi_bears_task_set.py
@@ -1,6 +1,4 @@
-import uuid
-
-from locust import SequentialTaskSet, task
+from locust import SequentialTaskSet, task, constant
 
 from domain.sqlalchemy import random_user
 from graphql.queries import notifications_by_user_id_query, get_all_stocks_query, get_all_metrics_query
@@ -8,46 +6,42 @@ from util.http_utils import create_headers, graphql_url, create_graphql_json
 
 
 class GummiBearsTaskSet(SequentialTaskSet):
+    wait_time = constant(1)
+    gummi_bear_user = None
 
-    # def __init__(self, *args, **kwargs):
-    #     super(SequentialTaskSet, self).__init__(*args, **kwargs)
-    #     Each locust user gets a different id
-        # self.random_id = str(uuid.uuid4())
+    def on_start(self):
+        self.gummi_bear_user = random_user()
 
-    # user = None
-
-    # def on_start(self):
-    #     self.user = random_user()
+    def on_stop(self):
+        self.gummi_bear_user = None
 
     @task
     def get_notifications(self):
-        user = random_user()
         query = notifications_by_user_id_query
-        variables = {'userId': user.id}
+        variables = {'userId': self.gummi_bear_user.id}
         with self.client.post(url=graphql_url(),
                               json=(create_graphql_json(query, variables)),
-                              headers=(create_headers(user.auth_token))) as response:
+                              headers=(create_headers(self.gummi_bear_user.auth_token))) as response:
             if response.status_code != 200:
                 response.failure()
 
     @task
     def get_all_stocks(self):
-        user = random_user()
         query = get_all_stocks_query
         variables = {}
         with self.client.post(url=graphql_url(),
                               json=(create_graphql_json(query, variables)),
-                              headers=(create_headers(user.auth_token))) as response:
+                              headers=(create_headers(self.gummi_bear_user.auth_token))) as response:
             if response.status_code != 200:
                 response.failure()
 
     @task
     def get_all_metrics(self):
-        user = random_user()
         query = get_all_metrics_query
         variables = {}
         with self.client.post(url=graphql_url(),
                               json=(create_graphql_json(query, variables)),
-                              headers=(create_headers(user.auth_token))) as response:
+                              headers=(create_headers(self.gummi_bear_user.auth_token))) as response:
             if response.status_code != 200:
                 response.failure()
+
